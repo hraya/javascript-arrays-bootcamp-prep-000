a16fb1e1c2f7ce35b15c78c6e5587a0750aba3ca
Jr <hraya@lssu.edu>
2018-12-28 06:15:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-const newArr = array.unshift(element)
+array.unshift(element)
   return newArr;
 }
 
